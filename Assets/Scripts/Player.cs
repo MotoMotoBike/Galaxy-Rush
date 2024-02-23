ee81af7ccdbbd8899f7b95742dd4ebf69a657aea
Kazanin Roman <krp@sib-net.ru>
2024-02-23 14:00:11 +0600

    build

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using DefaultNamespace;
-using Unity.VisualScripting;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
